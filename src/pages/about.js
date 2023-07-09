0cbc81932170122e0fd68ff3c3882c7af35ce953
sabi <zentixzaneic@gmail.com>
2023-07-09 17:15:07 -0500

    add about page

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import * as React from 'react'
+
+const AboutPage = () => {
+  return (
+    <main>
+      <h1>about me</h1>
+      <p>hi there! i'm the proud creator of this site, which i built with gatsby</p>
+    </main>
+  )
+}
+
+export const Head = () => <title>about me</title>
+
+export default AboutPage
