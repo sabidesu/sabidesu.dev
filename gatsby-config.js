f16ebbe89c3d6c42f91fb4664442ea6ca8c8f4a9
sabi <zentixzaneic@gmail.com>
2023-08-27 00:48:36 -0400

    add image to home page

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,8 @@ module.exports = {
     title: `sabi`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="home">
       <p>i'm making this by following the gatsby tutorial</p>
+      <StaticImage
+        alt="it's sabi playing beat saber!"
+        src="https://i.imgur.com/JH1aEQ2.png"
+      />
     </Layout>
   )
 }
