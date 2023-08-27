2520d7e1d65d9266a184a3806a17f4f77cad5390
sabi <zentixzaneic@gmail.com>
2023-08-27 02:17:54 -0400

    add list of blog posts

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,14 +1,33 @@
 import * as React from 'react'
+import { graphql } from 'gatsby'
 import Layout from '../components/layout'
 import Seo from '../components/seo'
 
-const BlogPage = () => {
+const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle="blog">
-      <p>my posts'll go here</p>
+      <ul>
+        {
+          data.allFile.nodes.map(node => (
+            <li key={node.name}>
+              {node.name}
+            </li>
+          ))
+        }
+      </ul>
     </Layout>
   )
 }
+
+export const query = graphql`
+  query {
+    allFile {
+      nodes {
+        name
+      }
+    }
+  }
+`
 
 export const Head = () => <Seo title="blog" />
 
