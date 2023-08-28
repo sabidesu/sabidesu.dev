5c1b23252c0429035c1f33e23339214516ea1d42
julian donald <jed5gpx@virginia.edu>
2023-08-28 11:45:30 -0400

    add sitemetadata hook and use in seo

--- src/components/seo.js
+++ src/components/seo.js
@@ -1,19 +1,32 @@
 import * as React from 'react'
-import { graphql, useStaticQuery } from 'gatsby'
+import { useSiteMetadata } from "../hooks/use-site-metadata"
 
-const Seo = ({title}) => {
-  const data = useStaticQuery(graphql`
-    query {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
+const Seo = ({ title, description, pathname, children }) => {
+  const { title: siteTitle, description: defaultDescription, image, siteUrl, twitterUsername } = useSiteMetadata()
+
+  const seo = {
+    title: `${title} | ${siteTitle}`,
+    description: description || defaultDescription,
+    image: `${siteUrl}${image}`,
+    url: `${siteUrl}${pathname || '/'}`,
+    twitterUsername,
+  }
+
+  console.log(seo)
 
   return (
-    <title>{title} | {data.site.siteMetadata.title}</title>
+    <>
+      <title>{seo.title}</title>
+      <meta name="description" content={seo.description} />
+      <meta name="image" content={seo.image} />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content={seo.title} />
+      <meta name="twitter:url" content={seo.url} />
+      <meta name="twitter:description" content={seo.description} />
+      <meta name="twitter:image" content={seo.image} />
+      <meta name="twitter:creator" content={seo.twitterUsername} />
+      {children}
+    </>
   )
 }
 
