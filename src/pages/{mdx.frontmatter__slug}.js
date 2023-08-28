d34a37127d9645c109eb89962e86ad8298251862
sabi <zentixzaneic@gmail.com>
2023-08-28 00:53:08 -0400

    create blog post page template

---
+++ src/pages/{mdx.frontmatter__slug}.js
@@ -0,0 +1,15 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+import Seo from '../components/seo'
+
+const BlogPost = () => {
+  return (
+    <Layout pageTitle="blog posts">
+      <p>blog post contents'll go here eventually</p>
+    </Layout>
+  )
+}
+
+export const Head = () => <Seo title="blog posts" />
+
+export default BlogPost
