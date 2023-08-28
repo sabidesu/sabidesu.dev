a284e75d8f50420b2d92e3b350b2346629bc23d1
sabi <zentixzaneic@gmail.com>
2023-08-28 19:14:43 -0400

    add protogen wallpaper to page

--- src/pages/protogens.js
+++ src/pages/protogens.js
@@ -1,11 +1,16 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 import Seo from '../components/seo'
 
 const ProtogensPage = () => {
   return (
     <Layout pageTitle="protogens!">
       <p>this page should automatically redirect you to the place where you can download the zip with the shit, but in case it didn't you can download it <a href="https://drive.google.com/file/d/1H6fxnCpdE888NsizKBDOyg6XrsUbc-TC/view?usp=drive_link">here</a></p>
+      <StaticImage
+        alt="a bunch of neon protogens over a dark green background, including an excited green one, sad purple one, angry orange one, and a happy blue one. there's binary reading 01010000, a computer with a P on the screen, RAM, and a tablet with 404 on the screen in the background"
+        src="../images/protogen_wallpaper.png"
+      />
     </Layout>
   )
 }
