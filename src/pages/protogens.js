c7236ef64245e820c2863b2dd5fc13ad1b7bdf63
sabi <zentixzaneic@gmail.com>
2023-08-28 21:03:37 -0400

    make it more obvious that the protogen design is mine

--- src/pages/protogens.js
+++ src/pages/protogens.js
@@ -18,7 +18,7 @@ const ProtogensPage = () => {
 export const Head = () => (
   <Seo
     pageTitle="protogens!"
-    description="download the protogen design here!"
+    description="download sabi's protogen design here!"
     image="/protogen_wallpaper.png"
   >
     <meta http-equiv="Refresh" content="0; URL=https://drive.google.com/file/d/1H6fxnCpdE888NsizKBDOyg6XrsUbc-TC/view?usp=drive_link" />
