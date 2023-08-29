ca5db07973276c953ea14846a80bd965c40540df
sabi <zentixzaneic@gmail.com>
2023-08-29 13:01:45 -0400

    attempt adding bootstrap cdn

--- src/components/seo.js
+++ src/components/seo.js
@@ -28,6 +28,8 @@ const Seo = ({ pageTitle, description, pathname, image, children }) => {
       <meta property="og:url" content={seo.url} />
       <meta property="og:description" content={seo.description} />
       <meta property="og:image" content={seo.image} />
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous" />
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
       {children}
     </>
   )
