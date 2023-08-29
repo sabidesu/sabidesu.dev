81e3a748519267cfab81384d8977b43d29f599f3
sabi <zentixzaneic@gmail.com>
2023-08-29 13:49:35 -0400

    attempt to style a navbar with bootstrap

--- src/components/layout.js
+++ src/components/layout.js
@@ -14,14 +14,21 @@ const Layout = ({ pageTitle, children }) => {
 
   return (
     <div>
-      <header>{data.site.siteMetadata.title}</header>
-      <nav>
-        <ul>
-          <li><Link to="/about">about</Link></li>
-          <li><Link to="/">home</Link></li>
-          <li><Link to="/blog">blog</Link></li>
-          <li><Link to="/protogens">protogens!</Link></li>
-        </ul>
+      <nav className="navbar navbar-expand-lg bg-dark text-white">
+        <div className="container-fluid">
+          <a className="navbar-brand">{data.site.siteMetadata.title}</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item"><Link to="/" className="nav-link">home</Link></li>
+              <li className="nav-item"><Link to="/about" className="nav-link">about</Link></li>
+              <li className="nav-item"><Link to="/blog" className="nav-link">blog</Link></li>
+              <li className="nav-item"><Link to="/protogens" className="nav-link">protogens!</Link></li>
+            </ul>
+          </div>
+        </div>
       </nav>
       <main>
         <h1>{pageTitle}</h1>
