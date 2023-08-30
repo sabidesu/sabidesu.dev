b0dd68984c54253fcaf5f2b56baf29d0c4329ad2
julian donald <jed5gpx@virginia.edu>
2023-08-30 12:27:38 -0400

    fix navbar occasionally being hidden behind main content

--- src/components/layout.js
+++ src/components/layout.js
@@ -14,7 +14,7 @@ const Layout = ({ pageTitle, children }) => {
 
   return (
     <div>
-      <nav className="navbar navbar-expand-sm text-bg-dark">
+      <nav className="navbar navbar-expand-sm text-bg-dark fixed-top">
         <div className="container-fluid">
           <h6 className="navbar-brand">{data.site.siteMetadata.title}</h6>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
