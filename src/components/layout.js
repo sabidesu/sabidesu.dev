3113dec149260a6981c98f55383c087b1930373c
sabi <zentixzaneic@gmail.com>
2023-08-30 21:23:07 -0400

    fix navbar situation

--- src/components/layout.js
+++ src/components/layout.js
@@ -16,12 +16,12 @@ const Layout = ({ pageTitle, children }) => {
     <div>
       <nav className="navbar navbar-expand-sm text-bg-dark fixed-top">
         <div className="container-fluid">
-          <h6 className="navbar-brand">{data.site.siteMetadata.title}</h6>
+          <h6 className="navbar-brand mb-1">{data.site.siteMetadata.title}</h6>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <ul className="navbar-nav me-auto mb-0">
               <li className="nav-item"><Link to="/" className="nav-link">home</Link></li>
               <li className="nav-item"><Link to="/about" className="nav-link">about</Link></li>
               <li className="nav-item"><Link to="/blog" className="nav-link">blog</Link></li>
