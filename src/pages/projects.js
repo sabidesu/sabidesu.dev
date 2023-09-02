dc42e369dc71ea1f1a5e9807b6075d0e406fc589
sabi <zentixzaneic@gmail.com>
2023-09-01 20:34:34 -0400

    get contentful working for showing projects

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,32 +1,44 @@
 import * as React from 'react'
+import { graphql } from 'gatsby'
 import Layout from '../components/layout'
 import Seo from '../components/seo'
 import { GatsbyImage } from 'gatsby-plugin-image'
 import { customButtons, btnOutlineDangerEmphasis } from '../styles/button_styles.module.css'
 
-const projects = [
-  {
-    name: "personal website",
-    description: "you're currently on it!",
-    viewLink: "https://youtu.be/dQw4w9WgXcQ?si=4LhkPvkMzEPds7m-",
-    sourceLink: "https://github.com/sabidesu/sabidesu.dev",
-    sourceLinkText: "github",
-    previewImage: null,
-  },
-]
+export const query = graphql`
+  query {
+    allContentfulProject {
+      nodes {
+        description {
+          description
+        }
+        name
+        previewImage {
+          file {
+            url
+          }
+          description
+        }
+        viewLink
+        sourceLink
+        sourceLinkText
+      }
+    }
+  }
+`
 
-const ProjectsPage = () => {
+const ProjectsPage = ({ data }) => {
   return (
     <Layout pageTitle="projects">
       <h3 className="mb-3">here are some projects i've worked on!</h3>
       {
-        projects.map((project) => <ProjectCard project={project} key={project.name} />)
+        data.allContentfulProject.nodes.map((project) => <ProjectCard project={project} key={project.name} />)
       }
     </Layout>
   )
 }
 
-const ProjectCard = ({project}) => {
+const ProjectCard = ({ project }) => {
   return (
     <div className="card mb-3">
       <div className="row g-0">
@@ -37,7 +49,7 @@ const ProjectCard = ({project}) => {
         <div className="col-sm">
           <div className="card-body">
             <h5 className="card-title">{project.name}</h5>
-            <p className="card-text">{project.description}</p>
+            <p className="card-text">{project.description.description}</p>
             <div className="d-flex justify-content-end" id={customButtons}>
               { project.sourceLink && <a href={project.sourceLink} className="btn btn-outline-light rounded-pill" target="_blank" rel="noopener noreferrer">{project.sourceLinkText}</a>}
               { project.viewLink && <a href={project.viewLink} className={`btn ${btnOutlineDangerEmphasis} rounded-pill ms-2`} target="_blank" rel="noopener noreferrer">view</a>}
